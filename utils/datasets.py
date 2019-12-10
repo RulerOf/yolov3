65b89cdb1cc0e0e3dc972051c5a37573cfc0540f
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2019-12-10 04:44:44 -0500

    Support glob source path input

--- utils/datasets.py
+++ utils/datasets.py
@@ -44,10 +44,12 @@ class LoadImages:  # for inference
     def __init__(self, path, img_size=416, half=False):
         path = str(Path(path))  # os-agnostic
         files = []
-        if os.path.isdir(path):
-            files = sorted(glob.glob(os.path.join(path, '*.*')))
-        elif os.path.isfile(path):
+        if os.path.isfile(path):
             files = [path]
+        elif os.path.isdir(path):
+            files = sorted(glob.glob(os.path.join(path, '*.*')))
+        else:
+            files = sorted(glob.glob(path))
 
         images = [x for x in files if os.path.splitext(x)[-1].lower() in img_formats]
         videos = [x for x in files if os.path.splitext(x)[-1].lower() in vid_formats]
